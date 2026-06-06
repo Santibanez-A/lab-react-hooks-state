e54bc0600e564314d1ae94f9200128af1135206b
Adrian Santibanez <santibanez1996@gmail.com>
2026-06-06 17:32:56 -0500

    Add vitest setup file

--- src/App.jsx
+++ src/App.jsx
@@ -35,7 +35,7 @@ const App = () => {
         <option value="all">All</option>
         <option value="Fruits">Fruits</option>
         <option value="Dairy">Dairy</option>
-        <option value="NonExistent">NonExistent</option>
+        <option value="Veggies">Veggies</option>
       </select>
 
       <ProductList selectedCategory={category} onAddToCart={handleAddToCart} />
